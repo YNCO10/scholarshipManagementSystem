446c3f52fc518dc3e9cdabff3dd24115e1d454a8
Yanco_Kampha <yancokamphandule@gmail.com>
2025-08-18 00:03:36 +0200

    added applicant login

--- SholarshipManagementSystem/authentications/regApplicantValidationPHP.py
+++ SholarshipManagementSystem/authentications/regApplicantValidationPHP.py
@@ -1,5 +1,8 @@
+import re
+
+
 from PyQt6.QtGui import QIcon
-from PyQt6.QtWidgets import QWidget, QMessageBox, QLineEdit, QApplication
+from PyQt6.QtWidgets import QWidget, QMessageBox
 import requests
 import json
 from SholarshipManagementSystem.authentications.applicantReg import Ui_applicantRegistration
@@ -14,6 +17,7 @@ class AppValCode(QWidget, Ui_applicantRegistration):
         self.setWindowTitle("REGISTER")
         self.setWindowIcon(QIcon("../../icons/SMsysIcon.png"))
         self.regCode = RegCode()
+        self.url = "http://localhost/BackEnd/scholarshipManagement/authentications/regApplicantValidation.php"
 
 
         #initialise errorMsgTxt
@@ -33,8 +37,25 @@ class AppValCode(QWidget, Ui_applicantRegistration):
         #add education levels
         self.educationCombo.addItems(self.educationLevels)
 
+        # add values for gender
+        self.genders = [
+            "Male",
+            "Female",
+            "Other"
+        ]
+        self.genderCombo.addItems(self.genders)
+
+        # values for spin box min, max & default
+        self.ageSpinBox.setMinimum(18)
+        self.ageSpinBox.setMaximum(40)
+        self.ageSpinBox.setValue(18)
+
+
+
+        # BTN CLICKS
         self.btnClicks()
 
+    # ALL BTN CLICKS BELOW
     def btnClicks(self):
         self.showHidePassBtn.clicked.connect(
             lambda : self.regCode.togglePassword(
@@ -50,10 +71,85 @@ class AppValCode(QWidget, Ui_applicantRegistration):
             )
         )
 
-    def goTologinPage(self):
-        pass #CONVERT A LOGIN PAGE FROM LOGIN.UI AND NAME IT APPLICANT_LOGIN
-        # from loginValidationPHP import LoginCode
-        # self.win = LoginCode()
-        # self.hide()
-        # self.win.show()
+        # go to loginPage
+        self.goToLoginPageBtn.clicked.connect(self.goToLoginPage)
 
+    #     register
+        self.signUpBtn.clicked.connect(self.register)
+
+
+    def register(self):
+
+        if self.nameTxt.text() == "" or self.nationalityTxt.text() == "" or self.passTxt.text() == "" or self.emailTxt.text() == "" or self.confirmPassTxt.text() == "":
+            self.msgBox("Blank Fields", "Please fill in all blank fields.")
+            return
+
+        if not self.regCode.validatePassField(self.emailTxt.text().strip()):
+            self.errorMsgLog.setText("Please enter valid Email")
+            return
+
+        if self.passTxt.text().strip() != self.confirmPassTxt.text().strip():
+            self.errorMsgLog.setText("The passwords do not match.")
+            return
+
+        if not self.regCode.validatePassField(self.passTxt.text().strip()):
+            self.errorMsgLog.setText("Password must be 8 characters long\nMust have at least one number\nAt least one symbol")
+            return
+
+        if not self.validatePhoneNumberLineEdit(self.phoneNumTxt.text().strip()):
+            self.errorMsgLog.setText("Phone number must start with + and contain 7–15 digits.")
+            return
+
+        dob = self.DOBdateEdit.date()
+        try:
+            response = requests.post(
+                self.url,
+                data={
+                    "name":self.nameTxt.text().strip(),
+                    "email":self.emailTxt.text().strip(),
+                    "nationality":self.nationalityTxt.text().strip(),
+                    "gender":self.genderCombo.currentText(),
+                    "phone_number":self.phoneNumTxt.text().strip(),
+                    "age":self.ageSpinBox.text(),
+                    "dob":dob.toPyDate(),
+                    "education_level":self.educationCombo.currentText(),
+                    "password":self.passTxt.text().strip()
+                }
+            )
+
+            result = json.loads(response.text)
+            print(f"Raw response: {response.text}")
+            errorMsg = result.get("message", "Unknown error")
+
+            if result.get("status") == "success":
+                self.msgBox("Welcome", "Enjoy your experience.")
+                print("Registration Successful")
+            else:
+                self.msgBox("Error", f"Error: {errorMsg}")
+                print(f"Something went wrong (Reg): {errorMsg}")
+
+
+        except Exception as e:
+            self.msgBox("Error", f"Oops something went wrong: {e}")
+            print(f"Oops something went wrong: {e}")
+
+
+    def validatePhoneNumberLineEdit(self, phoneNum):
+        pattern = r"^\+\d{7,15}$"
+
+        return re.match(pattern, phoneNum) is not None
+
+
+    def goToLoginPage(self):
+        from loginValidationPHP import LoginCode
+        self.win = LoginCode()
+        self.hide()
+        self.win.show()
+
+
+    def msgBox(self, title, msg):
+        msgBox = QMessageBox()
+        msgBox.setWindowTitle(title)
+        msgBox.setText(msg)
+        msgBox.setStandardButtons(QMessageBox.StandardButton.Ok)
+        msgBox.exec()
